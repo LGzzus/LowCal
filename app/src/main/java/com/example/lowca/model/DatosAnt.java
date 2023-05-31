4fbb6f4c2cdbbdab5ea5096e50f1d9026c6e7ae2
Jesus Adair Sosa Garcia <jesussosa9859@gmail.com>
2023-05-31 13:11:49 -0600

    [LGzzus]Editar datos

---
+++ app/src/main/java/com/example/lowca/model/DatosAnt.java
@@ -0,0 +1,58 @@
+package com.example.lowca.model;
+
+public class DatosAnt {
+    String genero;
+    int estatura;
+    int pesoO;
+    int pesoA;
+    String fechaN;
+    String actividadF;
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public int getEstatura() {
+        return estatura;
+    }
+
+    public void setEstatura(int estatura) {
+        this.estatura = estatura;
+    }
+
+    public int getPesoO() {
+        return pesoO;
+    }
+
+    public void setPesoO(int pesoO) {
+        this.pesoO = pesoO;
+    }
+
+    public int getPesoA() {
+        return pesoA;
+    }
+
+    public void setPesoA(int pesoA) {
+        this.pesoA = pesoA;
+    }
+
+    public String getFechaN() {
+        return fechaN;
+    }
+
+    public void setFechaN(String fechaN) {
+        this.fechaN = fechaN;
+    }
+
+    public String getActividadF() {
+        return actividadF;
+    }
+
+    public void setActividadF(String actividadF) {
+        this.actividadF = actividadF;
+    }
+}
