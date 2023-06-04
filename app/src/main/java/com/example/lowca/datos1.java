680b9cb004b925c294cf131cd141a404b63cbcf2
Jesus Adair Sosa Garcia <jesussosa9859@gmail.com>
2023-06-03 18:18:39 -0600

    [LGzzus] errores en pantallas de registrar listo

--- app/src/main/java/com/example/lowca/datos1.java
+++ app/src/main/java/com/example/lowca/datos1.java
@@ -1,14 +1,23 @@
 package com.example.lowca;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+
 public class datos1 extends AppCompatActivity {
-    EditText etPeso,etEstatura,etNacido;
+    TextInputLayout tilEstatura, tilPeso, tilFechaNa;
+    TextInputEditText etPeso,etEstatura,etNacido;
+    Button btnContinuar;
     String[] datos1;
     String[] datosDos;
     Bundle recibirDatos;
@@ -16,32 +25,78 @@ public class datos1 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_datos1);
-        etPeso=(EditText) findViewById(R.id.etPeso);
-        etEstatura=(EditText)findViewById(R.id.etEstatura);
-        etNacido=(EditText)findViewById(R.id.etNacido);
-
-
+        etPeso=(TextInputEditText) findViewById(R.id.etPeso);
+        etEstatura=(TextInputEditText) findViewById(R.id.etEstatura);
+        etNacido=(TextInputEditText) findViewById(R.id.etFechaNacido);
+        tilEstatura = (TextInputLayout) findViewById(R.id.TILEstatura);
+        tilPeso = (TextInputLayout) findViewById(R.id.TILPeso);
+        tilFechaNa = (TextInputLayout) findViewById(R.id.TILFechaN);
+        btnContinuar = (Button) findViewById(R.id.btnSiguiente);
         recibirDatos= getIntent().getExtras();
-    }
-    public void datos2Pantalla(View v){
 
+        btnContinuar.setOnClickListener(v -> {
+            try {
+                if(validar()){
+                    datos1=recibirDatos.getStringArray("keyDatos");
+                    String peso=etPeso.getText().toString();
+                    String estatura=etEstatura.getText().toString();
+                    String nacido=etNacido.getText().toString();
+                    datosDos= new String[]{peso,estatura,nacido};
+                    Bundle pasarDatos= new Bundle();
+                    Bundle pasarDatos2= new Bundle();
+                    pasarDatos.putStringArray("keyDatos",datos1);
+                    pasarDatos2.putStringArray("keyDatos2",datosDos);
 
-        datos1=recibirDatos.getStringArray("keyDatos");
-        String peso=etPeso.getText().toString();
-        String estatura=etEstatura.getText().toString();
-        String nacido=etNacido.getText().toString();
-        datosDos= new String[]{peso,estatura,nacido};
-        Bundle pasarDatos= new Bundle();
-        Bundle pasarDatos2= new Bundle();
-        pasarDatos.putStringArray("keyDatos",datos1);
-        pasarDatos2.putStringArray("keyDatos2",datosDos);
-
-       Intent intent = new Intent(this,datos2.class);
-        intent.putExtras(pasarDatos);
-        intent.putExtras(pasarDatos2);
-        startActivity(intent);
+                    Intent intent = new Intent(this,datos2.class);
+                    intent.putExtras(pasarDatos);
+                    intent.putExtras(pasarDatos2);
+                    startActivity(intent);
+                }else{
+                    Toast.makeText(this,"Llene todo",Toast.LENGTH_LONG).show();
+                }
+            }catch (Exception e){
+            }
+        });
+        etNacido.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openDialog();
+            }
+        });
     }
     public void atras(View view){
         this.finish();
+    }
+    public boolean validar(){
+        boolean retorno = true;
+        String peso=etPeso.getText().toString();
+        String estatura=etEstatura.getText().toString();
+        String fechan=etNacido.getText().toString();
+        if(peso.isEmpty()){
+            tilPeso.setError("Ingresa tu Peso Actual");
+        }else {
+            tilPeso.setErrorEnabled(false);
+        }
+        if (estatura.isEmpty() ) {
+            tilEstatura.setError("Ingresa tu Estatura");
+        }else{
+            tilEstatura.setErrorEnabled(false);
+        }
+        if (fechan.isEmpty()) {
+            tilFechaNa.setError("Ingresa tu Fecha de Nacimiento");
+            retorno = false;
+        }else{
+            tilFechaNa.setErrorEnabled(false);
+        }
+        return retorno;
+    }
+    public void openDialog(){
+        DatePickerDialog fechaNacido = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int day) {
+                etNacido.setText(String.valueOf(day)+"/"+String.valueOf(month+1)+"/"+String.valueOf(year));
+            }
+        }, 2023, 06, 03);
+        fechaNacido.show();
     }
 }
--- app/src/main/java/com/example/lowca/datos2.java
+++ app/src/main/java/com/example/lowca/datos2.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.Spinner;
@@ -19,6 +20,8 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
@@ -30,12 +33,16 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class datos2 extends AppCompatActivity {
-    EditText etPesoObjetivo;
+    TextInputEditText etPesoObjetivo;
+    TextInputLayout tilPesoObjetivo;
     Spinner spinnerDatos;
     RadioButton rbHombre,rbMujer;
-    String[] datos1; String[]datosDos; //,datos3;
+    String[] datos1; String[]datosDos;
     Bundle recibirDatos1,recibirDatos2;
     String nombre, correo, password,peso,estatura,nacido,userUid;
+
+    Button btnSiguiente;
+
     int pesoActual,pesoObjetivo2, estatura2;
     public FirebaseAuth mAuth;
     private FirebaseFirestore db;
@@ -45,10 +52,12 @@ public class datos2 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_datos2);
         FirebaseApp.initializeApp(this);
-        etPesoObjetivo=(EditText) findViewById(R.id.etPesoObjetivo);
+        etPesoObjetivo=(TextInputEditText) findViewById(R.id.etPesoObjetivo);
+        tilPesoObjetivo = (TextInputLayout) findViewById(R.id.TILPesoO);
         rbHombre=(RadioButton) findViewById(R.id.radioBtnHombre);
         rbMujer=(RadioButton) findViewById(R.id.radioBtnMujer);
         spinnerDatos=(Spinner) findViewById(R.id.spinnerDatos);
+        btnSiguiente = (Button) findViewById(R.id.btnSiguiente);
         String[] opciones={"Leve","Moderada","Energica"};
         ArrayAdapter<String> adapter= new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,
                 opciones);
@@ -56,7 +65,90 @@ public class datos2 extends AppCompatActivity {
         recibirDatos1=getIntent().getExtras();
         recibirDatos2=getIntent().getExtras();
         datos1=recibirDatos1.getStringArray("keyDatos");
+        btnSiguiente.setOnClickListener(v -> {
+            try {
+                if(cargarCuenta()){
+                    userUid = mAuth.getCurrentUser().getUid();
+                    System.out.println(userUid);
+                    //Cuenta
+                    nombre=datos1[0];
+                    correo=datos1[1];
+                    password=datos1[2];
+                    //Datos 1
+                    peso=datosDos[0];
+                    estatura=datosDos[1];
+                    nacido=datosDos[2];
 
+                    //cabiar datos a int
+                    pesoActual = Integer.parseInt(peso);
+                    estatura2 = Integer.parseInt(estatura);
+
+                    Map<String,Object>user=new HashMap<>();
+                    Map<String,Object>account=new HashMap<>();
+                    Map<String,Object>antropometric_dates=new HashMap<>();
+                    user.put("name",nombre);
+                    account.put("mail",correo);
+                    account.put("password",password);
+
+                    //String nivelAcF;
+                    String genero;
+                    String pesoObjetivo=etPesoObjetivo.getText().toString();
+                    pesoObjetivo2 = Integer.parseInt(pesoObjetivo);
+                    String nivelAcF= spinnerDatos.getSelectedItem().toString();
+
+                    antropometric_dates.put("weight", pesoActual);
+                    antropometric_dates.put("height",estatura2);
+                    antropometric_dates.put("target_weight",pesoObjetivo2);
+                    antropometric_dates.put("birth_date", nacido);
+                    antropometric_dates.put("physical_activity_lever",nivelAcF);
+
+
+                    if(rbMujer.isChecked()){
+                        genero="Mujer";
+                        antropometric_dates.put("gender",genero);
+                    } else if(rbHombre.isChecked()){
+                        genero="Hombre";
+                        antropometric_dates.put("gender",genero);
+                    }else {
+                        Toast.makeText(this,"Seleccione sus datos",Toast.LENGTH_LONG).show();
+                    }
+
+
+
+                    db.collection("account").document(userUid)
+                            .set(account).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void aVoid) {
+                                    Log.d(TAG, "DocumentSnapshot written with ID: ");
+                                }
+                            });
+                    db.collection("user").document(userUid)
+                            .set(user)
+                            .addOnSuccessListener(new OnSuccessListener<Void>() {
+
+                                @Override
+                                public void onSuccess(Void aVoid) {
+                                    Log.d(TAG, "DocumentSnapshot written with ID:");
+                                }
+                            });
+                    db.collection("antropometric_dates").document(userUid)
+                            .set(antropometric_dates)
+                            .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void aVoid) {
+                                    Log.d(TAG, "DocumentSnapshot written with ID:");
+                                }
+                            });
+
+                    Intent intent=new Intent(datos2.this, MainActivity.class);
+
+                    startActivity(intent);
+                }else{
+                }
+            }catch (Exception e){
+
+            }
+        });
 
         datosDos=recibirDatos2.getStringArray("keyDatos2");
 
@@ -65,9 +157,6 @@ public class datos2 extends AppCompatActivity {
         db = FirebaseFirestore.getInstance();
 
         onStart();
-
-
-
     }
     public void onStart() {
         super.onStart();
@@ -78,90 +167,20 @@ public class datos2 extends AppCompatActivity {
         }
     }
 
-    public void cargarCuenta(View view){
-        userUid = mAuth.getCurrentUser().getUid();
-        System.out.println(userUid);
-        //Cuenta
-        nombre=datos1[0];
-        correo=datos1[1];
-        password=datos1[2];
-        //Datos 1
-        peso=datosDos[0];
-        estatura=datosDos[1];
-        nacido=datosDos[2];
 
-        //cabiar datos a int
-        pesoActual = Integer.parseInt(peso);
-        estatura2 = Integer.parseInt(estatura);
-
-        Map<String,Object>user=new HashMap<>();
-        Map<String,Object>account=new HashMap<>();
-        Map<String,Object>antropometric_dates=new HashMap<>();
-        user.put("name",nombre);
-        account.put("mail",correo);
-        account.put("password",password);
-
-        //String nivelAcF;
-        String genero;
-        String pesoObjetivo=etPesoObjetivo.getText().toString();
-        pesoObjetivo2 = Integer.parseInt(pesoObjetivo);
-        String nivelAcF= spinnerDatos.getSelectedItem().toString();
-
-        antropometric_dates.put("weight", pesoActual);
-        antropometric_dates.put("height",estatura2);
-        antropometric_dates.put("target_weight",pesoObjetivo2);
-        antropometric_dates.put("birth_date", nacido);
-        antropometric_dates.put("physical_activity_lever",nivelAcF);
-
-
-        if(rbMujer.isChecked()){
-            genero="Mujer";
-            antropometric_dates.put("gender",genero);
-        } else if(rbHombre.isChecked()){
-            genero="Hombre";
-            antropometric_dates.put("gender",genero);
-        }else {
-            Toast.makeText(this,"Seleccione sus datos",Toast.LENGTH_LONG).show();
+    public boolean cargarCuenta(){
+        boolean retorno = true;
+        String pesoObjetivotxt = etPesoObjetivo.getText().toString();
+        if (pesoObjetivotxt.isEmpty()){
+            tilPesoObjetivo.setError("Ingrese su peso Objetivo");
+            retorno = false;
+        }else{
+            tilPesoObjetivo.setErrorEnabled(false);
         }
-
-
-
-        db.collection("account").document(userUid)
-                .set(account).addOnSuccessListener(new OnSuccessListener<Void>() {
-                    @Override
-                    public void onSuccess(Void aVoid) {
-                        Log.d(TAG, "DocumentSnapshot written with ID: ");
-                    }
-                });
-        db.collection("user").document(userUid)
-                .set(user)
-                .addOnSuccessListener(new OnSuccessListener<Void>() {
-
-                    @Override
-                    public void onSuccess(Void aVoid) {
-                        Log.d(TAG, "DocumentSnapshot written with ID:");
-                    }
-                });
-        db.collection("antropometric_dates").document(userUid)
-                .set(antropometric_dates)
-                .addOnSuccessListener(new OnSuccessListener<Void>() {
-                    @Override
-                    public void onSuccess(Void aVoid) {
-                        Log.d(TAG, "DocumentSnapshot written with ID:");
-                    }
-                });
-        /*System.out.println("Nombre: "+nombre+" Correo: "+correo+" Contraseña: "+contraseña);
-        System.out.println("Peso: "+peso+" Estatura"+ estatura+" Nacido: "+nacido);*/
-
-        Intent intent=new Intent(datos2.this, MainActivity.class);
-
-        startActivity(intent);
-
+        return retorno;
     }
 
     public void atras(View view){
         this.finish();
     }
-
-
 }
